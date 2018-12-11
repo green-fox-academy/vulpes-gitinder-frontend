f41ae652a19eb13271851b054dcbb745e4c28734
unknown <jan.splichal85@gmail.com>
2018-12-11 12:19:38 +0100

    project set-up

--- app/src/main/java/com/greenfox/gitinder/MainActivity.java
+++ app/src/main/java/com/greenfox/gitinder/MainActivity.java
@@ -2,12 +2,18 @@ package com.greenfox.gitinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello = findViewById(R.id.text);
     }
 }
