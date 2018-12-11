2c7060a1202c5fc294e57a02a1a29dc673131e75
unknown <jan.splichal85@gmail.com>
2018-12-11 18:38:31 +0100

    all test passing, feature done

--- app/src/test/java/com/greenfox/gitinder/activities/LoginTest.java
+++ app/src/test/java/com/greenfox/gitinder/activities/LoginTest.java
@@ -9,15 +9,18 @@ import android.content.SharedPreferences;
 import com.greenfox.gitinder.MainActivity;
 
 import org.apache.tools.ant.Main;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
+import org.robolectric.android.controller.ActivityController;
 import org.robolectric.shadow.api.Shadow;
 import org.robolectric.shadows.ShadowApplication;
 
 import androidx.test.core.app.ApplicationProvider;
+import retrofit2.http.PUT;
 
 import static org.junit.Assert.*;
 import static org.robolectric.Shadows.*;
@@ -26,6 +29,11 @@ import static org.robolectric.Shadows.*;
 public class LoginTest {
 
     Login login;
+
+    @Before
+    public void setup() {
+
+    }
 
     @Test
     public void buttonTextTest() {
@@ -36,9 +44,28 @@ public class LoginTest {
     public void testRedirectTrue() {
         SharedPreferences preferences =  ApplicationProvider.getApplicationContext().getSharedPreferences("preference", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("gitinder-token", "aaa").commit();
+        editor.putString("gitinder-token", "aaa").apply();
         login = Robolectric.setupActivity(Login.class);
         Intent expectedIntent = new Intent(login, MainActivity.class);
+        Intent actual = shadowOf((Application) ApplicationProvider.getApplicationContext()).getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actual.getComponent());
+    }
+    @Test
+    public void testRedirectFail() {
+        SharedPreferences preferences = ApplicationProvider.getApplicationContext().getSharedPreferences("preference", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("ryba", "aaa");
+        Intent actual = shadowOf((Application) ApplicationProvider.getApplicationContext()).getNextStartedActivity();
+        assertEquals(null, actual);
+    }
+    @Test
+    public void testButtonWillRedirect() {
+        SharedPreferences preferences =  ApplicationProvider.getApplicationContext().getSharedPreferences("preference", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("gitinder-token", "aaa").apply();
+        login = Robolectric.setupActivity(Login.class);
+        login.login.performClick();
+        Intent expectedIntent = new Intent(login, LoginGitHub.class);
         Intent actual = shadowOf((Application) ApplicationProvider.getApplicationContext()).getNextStartedActivity();
         assertEquals(expectedIntent.getComponent(), actual.getComponent());
     }
