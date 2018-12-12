8a7a3d6d64fd2c2817e839aecfd0e589d7426a43
Daniel <kolekd007@gmail.com>
2018-12-12 17:05:24 +0100

    Correct redirecting; Testing;

--- app/src/main/java/com/greenfox/gitinder/MainActivity.java
+++ app/src/main/java/com/greenfox/gitinder/MainActivity.java
@@ -1,27 +1,46 @@
 package com.greenfox.gitinder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.greenfox.gitinder.Model.Constants;
 import com.greenfox.gitinder.activities.Login;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView hello;
+    Button settingsButton;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        hello = findViewById(R.id.text);
+
+        settingsButton = findViewById(R.id.main_settings_button);
+
+        sharedPreferences = getSharedPreferences(Constants.SHARED_PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(Constants.GITINDER_TOKEN, "abc123").apply();
+
+        if (!sharedPreferences.contains(Constants.GITINDER_TOKEN)){
+            Log.d("token_checking", "Token is missing!");
+            toLogin();
+        } else {
+            Log.d("token_checking", "Token is present.");
+        }
     }
 
-    public void toLogin(View view) {
+    public void toLogin() {
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
     }
--- app/src/test/java/com/greenfox/gitinder/MainActivityTest.java
+++ app/src/test/java/com/greenfox/gitinder/MainActivityTest.java
@@ -1,11 +1,24 @@
 package com.greenfox.gitinder;
 
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.greenfox.gitinder.Model.Constants;
+import com.greenfox.gitinder.activities.Login;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
+import androidx.test.core.app.ApplicationProvider;
+
 import static org.junit.Assert.*;
+import static org.robolectric.Shadows.*;
+
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
@@ -13,9 +26,37 @@ public class MainActivityTest {
     private MainActivity mainActivity;
 
     @Test
-    public void helloWorldTest() {
+    public void helloButtonTest() {
         mainActivity = Robolectric.setupActivity(MainActivity.class);
-        assertEquals("Hello World!", mainActivity.hello.getText());
+        assertEquals("Settings", mainActivity.settingsButton.getText());
     }
 
+    @Test
+    public void tokenIsPresentRedirectionTest() {
+        SharedPreferences preferences = ApplicationProvider.getApplicationContext().getSharedPreferences(Constants.SHARED_PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(Constants.GITINDER_TOKEN, "abc123").apply();
+
+        mainActivity = Robolectric.setupActivity(MainActivity.class);
+        mainActivity.sharedPreferences.edit().putString(Constants.GITINDER_TOKEN, "abc123").apply();
+
+        Intent actualIntent = shadowOf((Application) ApplicationProvider.getApplicationContext()).getNextStartedActivity();
+
+        assertEquals(null, actualIntent);
+    }
+
+    @Test
+    public void tokenIsNotPresentRedirectionTest() {
+        SharedPreferences preferencesTest = ApplicationProvider.getApplicationContext().getSharedPreferences(Constants.SHARED_PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editorTest = preferencesTest.edit();
+        editorTest.clear();
+
+        mainActivity = Robolectric.setupActivity(MainActivity.class);
+        mainActivity.sharedPreferences.edit().clear().apply();
+
+        Intent expectedIntent = new Intent(mainActivity, Login.class);
+        Intent actualIntent = shadowOf((Application) ApplicationProvider.getApplicationContext()).getNextStartedActivity();
+
+        assertEquals(expectedIntent.getComponent(), actualIntent.getComponent());
+    }
 }
