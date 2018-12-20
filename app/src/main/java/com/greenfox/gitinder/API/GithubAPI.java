d05816a038951f2d38f6c8327e887d9f40f7ae2c
Daniel <kolekd007@gmail.com>
2018-12-20 10:55:11 +0100

    Applying requested changes

---
+++ app/src/main/java/com/greenfox/gitinder/API/GithubAPI.java
@@ -0,0 +1,15 @@
+package com.greenfox.gitinder.api;
+
+import com.greenfox.gitinder.api.model.GitHubToken;
+
+import retrofit2.Call;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface GithubAPI {
+
+    @Headers("Accept: application/json")
+    @POST("login/oauth/access_token")
+    Call<GitHubToken> getToken(@Query("client_id") String ClientID, @Query("client_secret") String secret, @Query("code") String code);
+}
--- app/src/main/java/com/greenfox/gitinder/API/GitinderAPI.java
+++ app/src/main/java/com/greenfox/gitinder/API/GitinderAPI.java
@@ -1,4 +1,4 @@
-package com.greenfox.gitinder.api;//package gitinder.backup.features.gitinder.clients;
+package com.greenfox.gitinder.api;
 
 import com.greenfox.gitinder.Model.Response.GitinderResponse;
 import com.greenfox.gitinder.Model.Response.LoginResponse;
@@ -10,16 +10,10 @@ import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
-import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
-import retrofit2.http.Query;
 
 public interface GitinderAPI {
-
-    @Headers("Accept: application/json")
-    @POST("login/oauth/access_token")
-    Call<com.greenfox.gitinder.api.model.GitHubToken> getToken(@Query("client_id") String ClientID, @Query("client_secret") String secret, @Query("code") String code);
 
     @POST("login")
     Call<LoginResponse> login(@Body User user);
