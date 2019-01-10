649e2d85666591893e03ca1a7cd473872d04ff69
unknown <jan.splichal85@gmail.com>
2019-01-10 11:50:53 +0100

    cleaned up the variants, added the server to dagger

--- app/src/main/java/com/greenfox/gitinder/dependencyInjection/module/AppModule.java
+++ app/src/main/java/com/greenfox/gitinder/dependencyInjection/module/AppModule.java
@@ -3,7 +3,11 @@ package com.greenfox.gitinder.dependencyInjection.module;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 
+import com.greenfox.gitinder.BuildConfig;
+import com.greenfox.gitinder.api.mock.BackendMockAPI;
+import com.greenfox.gitinder.api.service.GitinderAPI;
 import com.greenfox.gitinder.model.Settings;
 import com.greenfox.gitinder.Constants;
 import com.greenfox.gitinder.api.service.GithubAPI;
@@ -47,6 +51,26 @@ public class AppModule {
                 .addConverterFactory(GsonConverterFactory.create());
         Retrofit retrofit = builder.build();
         return retrofit.create(GithubAPI.class);
+    }
+    @Provides
+    @Singleton
+    GitinderAPI backendAPI() {
+        if (BuildConfig.FLAVOR.equals("live")) {
+            Retrofit.Builder builder = new Retrofit.Builder()
+                    .baseUrl("https://gitinder.azurewebsites.net")
+                    .addConverterFactory(GsonConverterFactory.create());
+            Retrofit retrofit = builder.build();
+            return retrofit.create(GitinderAPI.class);
+        } else if (BuildConfig.FLAVOR.equals("staging")) {
+            Retrofit.Builder builder = new Retrofit.Builder()
+                    .baseUrl("https://gitinder-staging.azurewebsites.net")
+                    .addConverterFactory(GsonConverterFactory.create());
+            Retrofit retrofit = builder.build();
+            return retrofit.create(GitinderAPI.class);
+        } else {
+            return new BackendMockAPI();
+        }
+
     }
 }
 
