3e3dc38b0f7b2b368bc43d9e96f0ae5ff1defed4
Daniel <kolekd007@gmail.com>
2019-01-11 15:14:12 +0100

    showing Marek's settings activity layout in the settings fragment

--- app/src/main/java/com/greenfox/gitinder/fragment/main/SettingsFragment.java
+++ app/src/main/java/com/greenfox/gitinder/fragment/main/SettingsFragment.java
@@ -16,7 +16,7 @@ public class SettingsFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+        View view = inflater.inflate(R.layout.activity_settings, container, false);
         return view;
     }
 }
