ce7582845dc860629d4d33a90759c86cc487644c
unknown <jan.splichal85@gmail.com>
2019-01-11 11:12:59 +0100

    created the testing button and added the dependencies for android design

--- app/src/main/java/com/greenfox/gitinder/activity/MainActivity.java
+++ app/src/main/java/com/greenfox/gitinder/activity/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.greenfox.gitinder.BuildConfig;
 import com.greenfox.gitinder.Constants;
 import com.greenfox.gitinder.R;
 
@@ -17,8 +18,10 @@ import javax.inject.Inject;
 import dagger.android.AndroidInjection;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     Button settingsButton;
+    Button profileButton;
 
     @Inject
     SharedPreferences sharedPreferences;
@@ -30,13 +33,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         settingsButton = findViewById(R.id.main_settings_button);
+        profileButton = findViewById(R.id.main_profile_button);
+        if (!BuildConfig.FLAVOR.equals("dev")) {
+            profileButton.setVisibility(View.GONE);
+        }
+
 
         if (!sharedPreferences.contains(Constants.GITINDER_TOKEN)){
-            Log.d("token_checking", "Token is missing!");
+            Log.d(TAG, "Token is missing!");
             toLogin();
         } else {
-            Log.d("token_checking", "Token is present.");
+            Log.d(TAG, "Token is present.");
         }
+
     }
 
     public void toLogin() {
@@ -46,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void toSettings(View view){
         Intent intent = new Intent(this,SettingsActivity.class);
+        startActivity(intent);
+    }
+    public void toProfile(View view) {
+        Intent intent = new Intent(this, ProfileDetails.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/greenfox/gitinder/activity/ProfileDetails.java
@@ -0,0 +1,14 @@
+package com.greenfox.gitinder.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.greenfox.gitinder.R;
+
+public class ProfileDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_details);
+    }
+}
