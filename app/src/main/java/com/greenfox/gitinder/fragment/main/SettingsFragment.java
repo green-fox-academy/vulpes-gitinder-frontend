3103b9f9949bf2ff39a9863afe3f3b5bc07ea06c
Marek Wimmer <marek.wimmer@gmail.com>
2019-01-18 11:35:15 +0100

    resolving conflict

--- app/src/main/java/com/greenfox/gitinder/fragment/main/SettingsFragment.java
+++ app/src/main/java/com/greenfox/gitinder/fragment/main/SettingsFragment.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.greenfox.gitinder.R;
 import com.greenfox.gitinder.fragment.BaseFragment;
