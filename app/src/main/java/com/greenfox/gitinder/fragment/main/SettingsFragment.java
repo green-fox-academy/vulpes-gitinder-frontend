66a1b10fdd8b5a513144a84b2a140ae79d508fdf
Daniel <kolekd007@gmail.com>
2019-01-21 13:40:47 +0100

    ..

--- app/src/main/java/com/greenfox/gitinder/fragment/main/SettingsFragment.java
+++ app/src/main/java/com/greenfox/gitinder/fragment/main/SettingsFragment.java
@@ -1,29 +1,35 @@
 package com.greenfox.gitinder.fragment.main;
 
-        import android.content.Intent;
-        import android.content.SharedPreferences;
-        import android.os.Bundle;
-        import android.support.annotation.NonNull;
-        import android.support.annotation.Nullable;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.Button;
-        import android.widget.CompoundButton;
-        import android.widget.ImageView;
-        import android.widget.SeekBar;
-        import android.widget.Switch;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import com.greenfox.gitinder.Constants;
-        import com.greenfox.gitinder.R;
-        import com.greenfox.gitinder.activity.MainActivity;
-        import com.greenfox.gitinder.fragment.BaseFragment;
-        import com.greenfox.gitinder.model.Settings;
-        import com.squareup.picasso.Picasso;
+import com.greenfox.gitinder.Constants;
+import com.greenfox.gitinder.R;
+import com.greenfox.gitinder.activity.MainActivity;
+import com.greenfox.gitinder.api.model.GitinderResponse;
+import com.greenfox.gitinder.api.service.GitinderAPI;
+import com.greenfox.gitinder.fragment.BaseFragment;
+import com.greenfox.gitinder.model.Settings;
+import com.squareup.picasso.Picasso;
 
-        import javax.inject.Inject;
+import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SettingsFragment extends BaseFragment implements CompoundButton.OnCheckedChangeListener {
     private static final String TAG = "SettingsFragment";
@@ -38,6 +44,8 @@ public class SettingsFragment extends BaseFragment implements CompoundButton.OnC
     SharedPreferences sharedPreferences;
     @Inject
     public Settings settings;
+    @Inject
+    GitinderAPI gitinderAPI;
 
 
     @Nullable
@@ -104,8 +112,22 @@ public class SettingsFragment extends BaseFragment implements CompoundButton.OnC
     }
 
     public void logout() {
-        sharedPreferences.edit().clear().apply();
         Intent intent = new Intent(getActivity(), MainActivity.class);
+        Call<GitinderResponse> call = gitinderAPI.logoutUser(sharedPreferences.getString(Constants.GITINDER_TOKEN, ""));
+
+        call.enqueue(new Callback<GitinderResponse>() {
+            @Override
+            public void onResponse(Call<GitinderResponse> call, Response<GitinderResponse> response) {
+                sharedPreferences.edit().clear().apply();
+
+            }
+
+            @Override
+            public void onFailure(Call<GitinderResponse> call, Throwable t) {
+
+            }
+        });
+
         startActivity(intent);
     }
 
